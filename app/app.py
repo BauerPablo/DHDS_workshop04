15c8957f2697cc2ba6d1ee28015f9c38b2524062
BauerPablo <pablo.bauer@aerolineas.com.ar>
2023-06-13 16:47:01 -0300

    dropzone funcionanado

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
+from flask_dropzone import Dropzone
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/data/uploads'
+UPLOAD_FOLDER = 'data/uploads'
 ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.update(
+	UPLOAD_FOLDER = UPLOAD_FOLDER,
+	DROPZONE_MAX_FILE_SIZE = 1024,
+	DROPZONE_TIMEOUT = 5*60*1000
+	)
+
+dropzone = Dropzone(app)
 
 @app.route('/')
 def index():
@@ -40,23 +47,31 @@ def pagina_no_encontrada(error):
 def allowed_file(filename):
 	return'.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/upload", methods=['GET', 'POST'])
 def upload_file():
-	if request.method == 'POST':
-		if 'file' not in request.files:
-			flash('Psss! no es el archivo')
-			return redirect(request.url)
 
-		file = request.files['file']
+	data = {
+	'titulo': 'DELIVERY PREDICTOR',
+	'area': 'Metrología'
+	}
+
+	if request.method == 'POST':
+		
+		file = request.files.get('file')
+		print(app.config['UPLOAD_FOLDER'])
+
+		if 'file' not in request.files:
+			flash('Psss! falta el archivo. Amigow!')
+			return render_template('index.html', data=data)
 
 		if file.filename == '':
 			flash('Psss! falta el archivo. Amigow!')
-			return redirect(request.url)
+			return render_template('index.html', data=data)
 
 		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('downlad_file', name=filename))
+			#filename = secure_filename(file.filename)
+			file.save(os.path.join('data/uploads', file.filename))
+			return render_template('index.html', data=data)
 
 def calculator():
 	pass
